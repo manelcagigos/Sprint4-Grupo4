4ec79a03a26157f0cc4f9153a5fbc20f518f6a93
Kangaroo13 <101421801+Kangaroo13@users.noreply.github.com>
2023-04-18 20:56:29 +0200

    inicio
    qwerghj

--- Sprint4/Sprint4/FormPrincipal.Designer.cs
+++ Sprint4/Sprint4/FormPrincipal.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Sprint4
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -30,12 +30,68 @@ namespace Sprint4
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cmb_portsD = new System.Windows.Forms.ComboBox();
+            this.btn_conn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(358, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 36);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cmb_portsD
+            // 
+            this.cmb_portsD.FormattingEnabled = true;
+            this.cmb_portsD.Location = new System.Drawing.Point(22, 22);
+            this.cmb_portsD.Name = "cmb_portsD";
+            this.cmb_portsD.Size = new System.Drawing.Size(121, 24);
+            this.cmb_portsD.TabIndex = 2;
+            this.cmb_portsD.SelectedIndexChanged += new System.EventHandler(this.cmb_portsD_SelectedIndexChanged);
+            // 
+            // btn_conn
+            // 
+            this.btn_conn.Location = new System.Drawing.Point(161, 23);
+            this.btn_conn.Name = "btn_conn";
+            this.btn_conn.Size = new System.Drawing.Size(75, 23);
+            this.btn_conn.TabIndex = 3;
+            this.btn_conn.Text = "Connectar";
+            this.btn_conn.UseVisualStyleBackColor = true;
+            this.btn_conn.Click += new System.EventHandler(this.btn_conn_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btn_conn);
+            this.Controls.Add(this.cmb_portsD);
+            this.Controls.Add(this.label1);
+            this.Name = "FormPrincipal";
+            this.Text = "2AUTH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox cmb_portsD;
+        private System.Windows.Forms.Button btn_conn;
     }
 }
 
--- Sprint4/Sprint4/FormPrincipal.cs
+++ Sprint4/Sprint4/FormPrincipal.cs
@@ -3,18 +3,101 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Sprint4
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        public FormPrincipal()
         {
             InitializeComponent();
+        }
+        string SecureCode;
+        int counter = 60;
+        SerialPort portArduino = new SerialPort();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports;
+
+            ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                cmb_portsD.Items.Add(port);
+            }
+
+            for (int i = 1; i < 7; i++)
+            {
+                int dig = numaleatori();
+                if (dig.ToString().StartsWith("-"))
+                {
+                    SecureCode += dig.ToString().Substring(1, 1);
+                }
+                else
+                {
+                    SecureCode += dig.ToString().Substring(0, 1);
+                }
+            }
+
+        }
+
+        private int numaleatori()
+        {
+            using (RNGCryptoServiceProvider rngCrypt = new RNGCryptoServiceProvider())
+
+            {
+                byte[] valor = new byte[4];
+
+                rngCrypt.GetBytes(valor);
+
+                return BitConverter.ToInt32(valor, 0);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+            {
+                timer1.Stop();
+                if (portArduino.IsOpen) portArduino.Close();
+                Application.Exit();
+            }
+            label1.Text = counter.ToString();
+        }
+
+        private void SendStartArdu()
+        {
+            portArduino.Open();
+            portArduino.Write("SA");
+            portArduino.Close();
+            //enviarmail
+
+        }
+
+        private void btn_conn_Click(object sender, EventArgs e)
+        {            
+            
+            SendStartArdu();
+
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 segundo
+            timer1.Start();
+            label1.Text = counter.ToString();
+
+        }
+
+        private void cmb_portsD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            portArduino = new SerialPort(cmb_portsD.SelectedItem.ToString(), 9600);
         }
     }
 }
